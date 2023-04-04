720136fc3f6842c0dff4a720052ab9b015fdfeaa
tolgahan <ulusoy.tolgahan@gmail.com>
2023-04-04 22:17:57 +0300

    Initial commit

---
+++ src/main/java/com/haydikodlayalim/rc/RestClientApplication.java
@@ -0,0 +1,21 @@
+package com.haydikodlayalim.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RestClientApplication {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientApplication.class, args);
+    }
+}
